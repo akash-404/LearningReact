9c1bde6a1977fda7eef4a514c4f55c7136bca7fe
akash-404 <akash.28.2011@gmail.com>
2025-01-04 00:23:24 +0530

    adde routing

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <div>    
+        <h1>Contact Us</h1>
+        <h2>Number: 1234567890</h2>
+        <h2>Email: samplemail@email.com</h2>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/RestaurantMenu.jsx
@@ -0,0 +1,17 @@
+const RestaurantMenu = () => {
+    return(
+        <div className="menu">
+            <h1>Name of the restaurant</h1>
+            <h2>Menu</h2>
+            <ul>
+                <li>Biryani</li>
+                <li>Burger</li>
+                <li>Wrap</li>
+                <li>Coke</li>
+                <li>Pizza</li>
+            </ul>
+        </div>
+    )
+}
+
+export default RestaurantMenu;
